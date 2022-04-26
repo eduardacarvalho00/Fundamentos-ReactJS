4484d2d7edbcf508e4553e28b5ec4a3eef3e2ba4
Eduarda Carvalho <eduardacarvalho9833@gmail.com>
2022-04-26 11:01:43 -0300

    React Project

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ReactRefreshWebpackplugin = require('@pmmmwh/react-refresh-webpack-plugin')
+
+const isDevelopement = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+    mode: isDevelopement ? 'development' : 'production',
+    devtool: isDevelopement ? 'eval-source-map' : 'source-map',
+    entry: path.resolve(__dirname, 'src', 'index.tsx'),
+    output:{
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    resolve:{
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    },
+    devServer: {
+        static: path.resolve(__dirname, 'public'),
+        hot:true,
+    },
+    plugins:[
+        isDevelopement && new ReactRefreshWebpackplugin(),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html')
+        })
+    ].filter(Boolean),
+    module: {
+        rules:[
+            {
+                test: /\.(j|t)sx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        plugins: [
+                            isDevelopement&& require.resolve('react-refresh/babel')
+                        ].filter(Boolean)
+                    }
+                }
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ],
+    }
+};
